2efe7b43438f3fbb58b64615d67580272b6e8018
justinJSwebdev <trankyhung225@gmail.com>
2025-01-21 11:13:12 +0700

    first commit

---
+++ src/Ex1.java
@@ -0,0 +1,10 @@
+public class Ex1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World in Ex1");
+        int a = 10;
+        float b = 5.0f;
+        char c = 'c';
+        boolean d = true;
+        String site = "Java";
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+    }
+}
